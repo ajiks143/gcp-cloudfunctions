c8b8f1b03438c53d78b3ed94dfd98182a8694cd1
Ajith Sasidharan <ajiks143@gmail.com>
2021-08-18 15:30:06 +0530

    Added comments to the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+# Command line argument Storage bucket to integrate with
 BUCKET_NAME=$1
 
+# CLI command to deploy Python Cloud function
 gcloud functions deploy gcs_file_metdata \
 --runtime python38 \
 --trigger-resource $BUCKET_NAME \
